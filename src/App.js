056a18b390bd8ea2c544a7e648a7e0a1420beb7e
thariqramadhan <athariqramadhan@yahoo.com>
2019-12-28 20:48:57 +0700

    Add routing in hats page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import HomepPage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+
 import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomepPage />
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </switch>
     </div>
   );
 }
